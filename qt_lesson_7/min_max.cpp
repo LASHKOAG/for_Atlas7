523b4469219594f2dd43481d10f68a317597527f
LASHKOAG <lashkoag@yandex.ru>
2023-02-26 12:12:29 +0600

    first commit

---
+++ qt_lesson_7/min_max.cpp
@@ -0,0 +1,21 @@
+#include "min_max.h"
+
+int get_min_from_array(int *_array, int _size)
+{
+
+}
+
+int get_max_from_array(int *_array, int _size)
+{
+    int max_value = _array[0];
+
+    for(int i = 1; i<_size; ++i)
+    {
+        if(_array[i] > max_value)
+        {
+            max_value = _array[i];
+        }
+    }
+
+    return max_value;
+}
